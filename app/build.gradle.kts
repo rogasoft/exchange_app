7c5b38a29070723db1ae4e8554248fbdcee14a5a
przemyslaw <przemek5504@gmail.com>
2026-01-09 17:01:00 +0100

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
 	alias(libs.plugins.android.application)
 	alias(libs.plugins.kotlin.android)
 	alias(libs.plugins.kotlin.compose)
+	alias(libs.plugins.hilt)
+	alias(libs.plugins.ksp)
 }
 
 android {
@@ -37,21 +39,16 @@ android {
 		compose = true
 	}
 }
-
 dependencies {
-	implementation(libs.androidx.core.ktx)
-	implementation(libs.androidx.lifecycle.runtime.ktx)
-	implementation(libs.androidx.activity.compose)
-	implementation(platform(libs.androidx.compose.bom))
-	implementation(libs.androidx.compose.ui)
-	implementation(libs.androidx.compose.ui.graphics)
-	implementation(libs.androidx.compose.ui.tooling.preview)
-	implementation(libs.androidx.compose.material3)
-	testImplementation(libs.junit)
-	androidTestImplementation(libs.androidx.junit)
-	androidTestImplementation(libs.androidx.espresso.core)
-	androidTestImplementation(platform(libs.androidx.compose.bom))
-	androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-	debugImplementation(libs.androidx.compose.ui.tooling)
-	debugImplementation(libs.androidx.compose.ui.test.manifest)
+	with(libs) {
+		implementation(platform(androidx.compose.bom))
+		implementation(platform(okhttp.bom))
+		implementation(bundles.core)
+		implementation(bundles.di)
+		implementation(bundles.network)
+		implementation(bundles.ui)
+		implementation(bundles.debugging)
+		testImplementation(bundles.testing)
+		ksp(hilt.compiler)
+	}
 }
